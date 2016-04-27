7725d1abfed0857c9f9984940d5aa2fe6ef36271
Rui Miguel Silva <rui.silva@linaro.org>
2016-04-27 11:15:25 +0100

    package: add gbutils package
    To avoid the need to always sync lsgb every time there is
    change, add a new package that will clone the github projectara
    gb-utils repository and will install lsgb to the target
    rootfs.
    
    Remove also the previous version of lsgb in the overlay section.
    
    By default it will clone the github repo, however the makefile
    include some commented lines that will allow to use a local
    clone instead for development.
    
    Signed-off-by: Rui Miguel Silva <rui.silva@linaro.org>

---
+++ package/gbutils/gbutils.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# gbutils
+#
+################################################################################
+
+GBUTILS_VERSION = master
+GBUTILS_LICENSE = LGPLv2.1
+GBUTILS_LICENSE_FILES = COPYING
+
+GBUTILS_SITE = $(call github,projectara,gb-utils,$(GBUTILS_VERSION))
+# Uncomment the lines bellow and comment line above if you want to use a local
+# clone of the gbutils
+#GBUTILS_SITE = $(GBUTILSDIR)
+#GBUTILS_SITE_METHOD = local
+
+define GBUTILS_INSTALL_TARGET_CMDS
+	cd $(@D) && cp lsgb $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(generic-package))
