e4cb7cf58dd92f247c6dbe81b98adff37c39da58
Bryan O'Donoghue <pure.logic@nexus-software.ie>
2016-11-29 00:12:14 +0000

    package: add greybus_loopback_test package
    The loopback_test utility allows various different types of bandwidth and
    latency tests to be performed against a greybus loopback Interface. This
    utility is available in the kernel tree as loopback_test, this package is
    called greybus_loopback_test here to fit in with the other package names in
    the greybus overlay.
    
    Once built this package will build the utility in
    drivers/staging/greybus/tools and install it to /usr/sbin/loopback_test in
    the target buildroot filesystem.
    
    Signed-off-by: Bryan O'Donoghue <pure.logic@nexus-software.ie>

---
+++ package/greybus_loopback_test/greybus_loopback_test.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# greybus_loopback_test
+#
+################################################################################
+
+GREYBUS_LOOPBACK_DIR_STAGING = $(LINUX_DIR)/drivers/staging/greybus
+GREYBUS_LOOPBACK_TEST_INSTALL_TARGET = YES
+
+define GREYBUS_LOOPBACK_TEST_BUILD_CMDS
+	$(Q)if test ! -f $(GREYBUS_LOOPBACK_DIR_STAGING)/tools/Makefile ; then \
+		echo "Your kernel version is too old and does not have the greybus loopback tool." ; \
+		echo "At least kernel 4.8 must be used." ; \
+		exit 1 ; \
+	fi
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(GREYBUS_LOOPBACK_DIR_STAGING)/tools \
+		loopback_test
+endef
+
+define GREYBUS_LOOPBACK_TEST_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(GREYBUS_LOOPBACK_DIR_STAGING)/tools/loopback_test \
+		$(TARGET_DIR)/usr/sbin
+endef
+
+$(eval $(generic-package))
